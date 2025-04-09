8a3ddaa312828a386b591d741f994cb1795284f4
Alex Kryzhanovskyi <kryzhanoskyi.oleksandr@gmail.com>
2025-04-09 20:56:28 +0300

    sortSolution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,43 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const arr = this;
+    const n = arr.length;
+
+    const compareFunction1 =
+      compareFunction ||
+      function (a, b) {
+        const aStr = String(a);
+        const bStr = String(b);
+
+        const aIsUpper = /^[A-Z]/.test(aStr);
+        const bIsUpper = /^[A-Z]/.test(bStr);
+
+        if (aIsUpper && !bIsUpper) {
+          return -1;
+        }
+
+        if (!aIsUpper && bIsUpper) {
+          return 1;
+        }
+        return aStr.localeCompare(bStr);
+      };
+
+    let swapped;
+
+    do {
+      swapped = false;
+
+      for (let i = 0; i < n - 1; i++) {
+        if (compareFunction1(arr[i], arr[i + 1]) > 0) {
+          [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+          swapped = true;
+        }
+      }
+    } while (swapped);
+
+    return arr;
   };
 }
 
